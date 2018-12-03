582a878ed74f082c45605ad4f94fa0bf36fda92a
Iolanda Chagas <iolanda.paiva@lais.huol.ufrn.br>
2018-12-03 15:09:52 -0300

    FIX: Corrige problemas com inserção da classe MagosGame

---
+++ src/magos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "../include/magos.h"
+
+namespace mzr
+{
+    
+    void MagosGame::welcome(){}
+
+    MagosGame::ResultType MagosGame::initialize(int argc, char ** argv)
+    {
+        ResultType teste;
+        return teste;
+    }
+
+    void MagosGame::process_events(){}
+    void MagosGame::update(){}
+    void MagosGame::render(){}
+
+    bool MagosGame::game_over()
+    {return true;}
+}
